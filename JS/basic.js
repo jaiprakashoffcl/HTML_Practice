5ef4888ecd7f3e61f706710d2e60fe15b25ba7cb
Jai Prakash <jaiprakash292033@gmail.com>
2025-09-03 10:54:21 +0530

    Updated files

---
+++ JS/basic.js
@@ -0,0 +1,26 @@
+let personname= "JP";
+{
+    console.log(personname);
+}
+
+let no=12344;
+{
+    console.log(no);
+    console.log(typeof(no));
+}
+
+let status=false;
+{
+    console.log(typeof(status));
+    console.log(status);
+}
+
+let name1=null;
+{
+    console.log(name1);
+}
+
+let declare;
+{
+    console.log(declare);
+}
---
+++ JS/basic2.js
@@ -0,0 +1,19 @@
+const course=['Python', 'Java', 'C', 'MERN','DA']
+{
+    console.log(course);
+    console.log(typeof(course));
+}
+
+const user = {
+    names:'jai prakash',
+    location:'trichy',
+    age:22,
+    email:'jaiprakash292003@mail.com',
+    phone: 7871553239
+};
+
+
+{
+    console.log(user);
+    console.log(typeof(user));
+}

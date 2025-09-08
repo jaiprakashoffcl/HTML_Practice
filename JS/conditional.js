f220302c4ebfeb40ffa158156a6929c6751386e8
Jai Prakash <jaiprakash292033@gmail.com>
2025-09-08 09:53:30 +0530

    javascript updates

--- JS/conditional.js
+++ JS/conditional.js
@@ -2,3 +2,23 @@ str='javascript'
 for (let i=0;i<str.length;i++){
     console.log(str[i])
 }
+
+//homogeneous data type
+const course=['py','js','da','fs','java']
+for(let k=0;k<str.length;k++){
+    console.log(course[k])
+}
+
+//heterogeneous data type
+
+const hetro =['bb', 'gt', 45,3.2,'we',112]
+for(let j=0;j<str.length;j++){
+    console.log(hetro[j])
+}
+
+//reverse string
+
+str='reverse'
+for(let a=str.length-1;a>=0;a--){
+    console.log(str[a])
+}
